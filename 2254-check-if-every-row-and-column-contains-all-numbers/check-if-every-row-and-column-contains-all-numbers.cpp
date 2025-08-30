59dc7aae3bb4d667601fa83ff4038bd0dd22cacc
Sandipan pal <palsandipan68@gmail.com>
2025-08-30 21:24:30 +0530

    Time: 4 ms (87.43%) | Memory: 43.4 MB (64.59%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -5,8 +5,6 @@ public:
         int n = mat.size();
 
         for(int i = 0; i < n; i++){
-            // int rowArr[n+1] = {0};
-            // int colArr[n+1] = {0};
             vector<int> rowArr(n+1, 0);
             vector<int> colArr(n+1, 0);
             for(int j = 0; j < n; j++){
