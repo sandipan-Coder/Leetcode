f7d5b5b6f96b1803347cbfe81cbf1eeecc6a2c90
Sandipan pal <palsandipan68@gmail.com>
2025-09-05 09:33:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (84.36%) - LeetSync

--- 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int minOperations(int n) {
         
-        if(n == 0 || n == 1)
-            return n;
-        
-        int val = 1;
-        while(val * 2 < n)
-            val *= 2;
-        
-        return 1 + min(minOperations(val*2 - n), minOperations(n - val));
+        int res = 0;
+        for(int i = 0; i < 17; i++){
 
+            if(__builtin_popcount(n + (1 << i)) < __builtin_popcount(n)){
+                n += (1 << i);
+                res++;
+            }
+        }
+
+        return res + __builtin_popcount(n);
     }
 };
