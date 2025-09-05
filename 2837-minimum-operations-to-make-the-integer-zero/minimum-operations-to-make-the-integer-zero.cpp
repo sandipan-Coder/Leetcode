539cc1e55bb86fad428b3b21ba672d392eba66ed
Sandipan pal <palsandipan68@gmail.com>
2025-09-05 10:11:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.57%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int makeTheIntegerZero(int num1, int num2) {
         
+        if(num2 > num1)
+            return -1;
+
         for(int i = 0; i <= 60; i++){
 
             long long int target = num1 - i*1LL*num2;
-
             if(target <= 0)
                 return -1;
 
