f27dc877138d7eea734cdfe483e3e111e08b9b77
Sandipan pal <palsandipan68@gmail.com>
2025-09-06 13:19:52 +0530

    Time: 8 ms (90.58%) | Memory: 183.2 MB (78.99%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -1,25 +1,42 @@
 class Solution {
+private:
+
+    long long solve(int l, int r){
+
+        long long L = 1;
+        long long S = 1;
+        long long steps = 0;
+
+        while(L <= r){
+
+            long long R = L*4 - 1;
+
+            long long start = max(L, (long long)l);
+            long long end = min(R, (long long)r);
+
+            if(start <= end)
+                steps += (end - start + 1) * S;
+            
+            S++;
+            L *= 4;
+        }
+
+        return steps;
+    }
+
 public:
     long long minOperations(vector<vector<int>>& queries) {
         
-         long long ans = 0;
-        vector<long long> p(16);
-        p[0] = 1;
+        long long ans = 0;
 
-        // Precompute powers of 4 up to 4^15
-        for (int i = 1; i < 16; i++) 
-            p[i] = p[i - 1] * 4;
+        for(auto &querie: queries){
 
-        for (const auto& q : queries) {
-            long long l = q[0], r = q[1], sum = 0;
-            // Iterate over powers of 4 to compute operations
-            for (int t = 1; t <= 15; t++) {
-                long long L = max(l, p[t - 1]);
-                long long R = min(r, p[t] - 1);
-                if (L <= R) 
-                    sum += t * (R - L + 1);
-            }
-            ans += (sum + 1) / 2;
+            int l = querie[0];
+            int r = querie[1];
+
+            long long steps = solve(l, r);
+
+            ans += (steps + 1) / 2;
         }
 
         return ans;
