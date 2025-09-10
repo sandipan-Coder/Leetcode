7cf3d26ae29aad04451245fa5512ba4d893d0036
Sandipan pal <palsandipan68@gmail.com>
2025-09-10 18:19:58 +0530

    Time: 15 ms (92.94%) | Memory: 65.6 MB (85.88%) - LeetSync

--- 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.cpp
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.cpp
@@ -7,16 +7,24 @@ public:
         for(int i = 0; i < friendships.size(); i++){
             int user1 = friendships[i][0];
             int user2 = friendships[i][1];
-            bool flag = false;
+            vector<bool> visited(n+1, false);
 
+            // Store all languages that is known by user1;
             for(int j = 0; j < languages[user1 - 1].size(); j++){
                 
                 int lang = languages[user1 - 1][j];
-                int cnt = count(languages[user2 - 1].begin(), languages[user2 - 1].end(), lang);
-                if(cnt > 0){
+                visited[lang] = true;
+            }
+
+            // Check if user2 know any of the language of user1;
+            bool flag = false;
+            for(int j = 0; j < languages[user2 - 1].size(); j++){
+                
+                int lang = languages[user2 - 1][j];
+                if(visited[lang]){
                     flag = true;
                     break;
-                }     
+                }
             }
 
             if(!flag){
@@ -25,19 +33,20 @@ public:
             } 
         }
 
+        if(st.size() == 0)
+            return 0;
+
         // Step2 :- Store the frequencies of languages of this friendships which have not any common language.
-        unordered_map<int, int> mp;
-        for(auto it: st){
+        vector<int> freq(n+1, 0);
+        for(int it: st){
             for(int i = 0; i < languages[it - 1].size(); i++)
-                mp[languages[it - 1][i]]++;
+                freq[languages[it - 1][i]]++;
         }
 
         // Step3 :- Find the maximum frequency of language
         int count = 0;
-        for(auto it: mp){
-            int noOfKnownPeople = it.second;
-            count = max(count, noOfKnownPeople);
-        }
+        for(int ele: freq)
+            count = max(count, ele);
 
         // Step4 :- Difference of No. of users who are friends but don't have any common language with maximum frequency language.
         int unCommon = st.size();
