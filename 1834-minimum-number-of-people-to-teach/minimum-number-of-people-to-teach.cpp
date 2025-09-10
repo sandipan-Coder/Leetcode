7eaf8fe4670c4b46ce7d02e9a1e9ba1b28efeebb
Sandipan pal <palsandipan68@gmail.com>
2025-09-10 18:22:27 +0530

    Time: 0 ms (100.00%) | Memory: 63.7 MB (100.00%) - LeetSync

--- 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.cpp
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.cpp
@@ -1,4 +1,6 @@
 class Solution {
+    bitset<501> visited;
+    int freq[501] = {0};
 public:
     int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
         // Step1 :- Store this friendships which have not any common language.
@@ -7,13 +9,14 @@ public:
         for(int i = 0; i < friendships.size(); i++){
             int user1 = friendships[i][0];
             int user2 = friendships[i][1];
-            vector<bool> visited(n+1, false);
+            // vector<bool> visited(n+1, false);
+            visited.reset();
 
             // Store all languages that is known by user1;
             for(int j = 0; j < languages[user1 - 1].size(); j++){
                 
                 int lang = languages[user1 - 1][j];
-                visited[lang] = true;
+                visited[lang] = 1;
             }
 
             // Check if user2 know any of the language of user1;
@@ -37,7 +40,7 @@ public:
             return 0;
 
         // Step2 :- Store the frequencies of languages of this friendships which have not any common language.
-        vector<int> freq(n+1, 0);
+        // vector<int> freq(n+1, 0);
         for(int it: st){
             for(int i = 0; i < languages[it - 1].size(); i++)
                 freq[languages[it - 1][i]]++;
