ad3ad3115c3fbaedd0509a5ea9bf5a20a9ca0f7d
Sandipan pal <palsandipan68@gmail.com>
2025-09-11 08:18:07 +0530

    Time: 16 ms (49.09%) | Memory: 15.7 MB (78.32%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,26 +1,31 @@
 class Solution {
+private:
+
+    bool isVowel(char ch){
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            return true;
+        else
+            return false;
+    }
+
 public:
     string sortVowels(string s) {
         
-        int n = s.length();
-        string res = "";
         vector<char> vowels;
 
         for(char ch: s){
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            if(isVowel(ch))
                 vowels.push_back(ch);
         }
 
         sort(begin(vowels), end(vowels));
         int idx = 0;
 
-        for(char ch: s){
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-                res += vowels[idx++];
-            else
-                res += ch;
+        for(char &ch: s){
+            if(isVowel(ch))
+               ch = vowels[idx++];
         }
 
-        return res;
+        return s;
     }
 };
