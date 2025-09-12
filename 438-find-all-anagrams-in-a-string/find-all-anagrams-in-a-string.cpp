11117e262ad28e7b85e88cac6fd84893d8cd6f92
Sandipan pal <palsandipan68@gmail.com>
2025-09-12 13:08:19 +0530

    Time: 3 ms (81.39%) | Memory: 11.6 MB (64.34%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        
+        vector<int> ans;
+        int n = s.size();
+        int m = p.size();
+
+        if(m > n)
+            return ans;
+            
+        vector<int> target(26, 0);
+        vector<int> window(26, 0);
+
+        // Count frequencies of each character in the string P.
+        for(char ch: p)
+            target[ch - 'a']++;
+
+        // Check the first window of size m which has same frequencies character or not.
+        for(int i = 0; i < m; i++)
+            window[s[i] - 'a']++;
+        
+        // Check if the substring is Anagram or not
+        if(target == window)
+            ans.push_back(0);
+
+        // Check if the remaining string has any Anagram or not.
+        for(int i = m; i < n; i++){
+
+            // Delete the last window element
+            window[s[i - m] - 'a']--;
+            // Add element this window
+            window[s[i] - 'a']++;
+
+            // Check if this substring is Anagram or not.
+            if(target == window)
+                ans.push_back(i - m + 1);
+        }
+
+        return ans;
+    }
+};
