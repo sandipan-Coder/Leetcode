d30729bea38ea11457996098f8918d1f08190adf
Sandipan pal <palsandipan68@gmail.com>
2025-09-13 11:10:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (65.92%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -2,9 +2,41 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
+        // Process :- 1
+        /*
+            int ans = 0;
+            int n = nums.size();
+
+            for(int bit = 0; bit < 32; bit++){
+                int count = 0;
+                for(int i = 0; i < n; i++){
+                    if(nums[i] & (1 << bit))
+                        count++;
+                }
+
+                if(count % 3)
+                    ans |= (1 << bit);
+            }
+
+            return ans;
+        */
+
+        // Process :- 2
+
+            sort(nums.begin(), nums.end());
+            int n = nums.size();
+            for(int i = 1; i < n; i += 3){
+                if(nums[i] != nums[i - 1])
+                    return nums[i - 1];
+            }
+
+            return nums[n - 1];
+
+
+        // Process :- 3
+        /*
         int one = 0;
         int two = 0;
-
         for(int ele: nums){
 
             one = (one ^ ele) & (~ two);
@@ -12,5 +44,6 @@ public:
         }
 
         return one;
+        */
     }
 };
