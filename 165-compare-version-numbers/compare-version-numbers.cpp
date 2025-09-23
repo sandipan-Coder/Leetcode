e2f713508c2f557535e9835c27fdd88fbe642f43
Sandipan pal <palsandipan68@gmail.com>
2025-09-23 07:15:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (69.84%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -6,49 +6,49 @@ public:
         int n2 = version2.length();
         int i = 0;
         int j = 0;
-        vector<int> ver1;
-        vector<int> ver2;
 
-        while(i < n1){
-            int num = 0;
+        while(i < n1 && j < n2){
+            int num1 = 0;
             while(i < n1 && version1[i] != '.'){
-                num = (num * 10) + (version1[i] - '0');
+                num1 = (num1 * 10) + (version1[i] - '0');
                 i++;
             }
-            ver1.push_back(num);
-            i++;
-        }
-       
-        while(j < n2){
-            int num = 0;
+
+            int num2 = 0;
             while(j < n2 && version2[j] != '.'){
-                num = (num * 10) + (version2[j] - '0');
+                num2 = (num2 * 10) + (version2[j] - '0');
                 j++;
             }
-            ver2.push_back(num);
-            j++;
-        }
 
-        i = 0, j = 0;
-
-        while(i < ver1.size() && j < ver2.size()){
-            
-            if(ver1[i] < ver2[j])
+            if(num1 > num2)
+                return 1;
+            else if(num1 < num2)
                 return -1;
-            else if(ver1[i] > ver2[j])
-                return 1;
+
             i++;
             j++;
         }
+       
+        while(i < n1){
+            int num1 = 0;
+            while(i < n1 && version1[i] != '.'){
+                num1 = (num1 * 10) + (version1[i] - '0');
+                i++;
+            }
 
-        while(i < ver1.size()){
-            if(ver1[i] > 0)
+            if(num1)
                 return 1;
             i++;
         }
 
-        while(j < ver2.size()){
-            if(ver2[j] > 0)
+        while(j < n2){
+            int num2 = 0;
+            while(j < n2 && version2[j] != '.'){
+                num2 = (num2 * 10) + (version2[j] - '0');
+                j++;
+            }
+
+            if(num2)
                 return -1;
             j++;
         }
