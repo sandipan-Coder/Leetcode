0b71d90e982fbd3d18fcbd0b8a4475a7697e81b9
Sandipan pal <palsandipan68@gmail.com>
2025-10-06 08:45:10 +0530

    Time: 7 ms (62.42%) | Memory: 13 MB (71.24%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -3,33 +3,45 @@ public:
     int swimInWater(vector<vector<int>>& grid) {
         
         int m = grid.size(), n = grid[0].size();
-        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> pq;
         vector<pair<int,int>> directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        set<pair<int,int>> seen;
         
-        pq.push({grid[0][0], 0, 0});
-        
-        while (!pq.empty()) {
-            auto [max_d, r, c] = pq.top();
-            pq.pop();
-            
-            if (seen.count({r, c})) 
-                continue;
-
-            seen.insert({r, c});
-            
+        auto dfs = [&](int r, int c, int mid, vector<vector<bool>>& seen, auto& dfs_ref) -> bool {
             if (r == m-1 && c == n-1) 
-                return max_d;
+                return true;
+                
+            seen[r][c] = true;
             
             for (auto [dr, dc] : directions) {
                 int nr = r + dr, nc = c + dc;
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n && !seen.count({nr, nc})) {
-                    int new_d = max(max_d, grid[nr][nc]);
-                    pq.push({new_d, nr, nc});
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && !seen[nr][nc]) {
+                    if (grid[nr][nc] <= mid) {
+                        if (dfs_ref(nr, nc, mid, seen, dfs_ref)) 
+                            return true;
+                    }
                 }
             }
+            return false;
+        };
+        
+        auto possible = [&](int mid) {
+            if (grid[0][0] > mid) return false;
+            vector<vector<bool>> seen(m, vector<bool>(n, false));
+            return dfs(0, 0, mid, seen, dfs);
+        };
+        
+        int lo = grid[0][0], hi = 0;
+        for (auto& row : grid)
+            for (int val : row)
+                hi = max(hi, val);
+        
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (possible(mid))
+                hi = mid;
+            else 
+                lo = mid + 1;
         }
 
-        return -1;
+        return lo;
     }
 };
