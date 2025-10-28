b92637ae0de4290b026158f53c087adc77b52d8b
Sandipan pal <palsandipan68@gmail.com>
2025-10-28 19:43:05 +0530

    Time: 4 ms (68.35%) | Memory: 22.1 MB (67.89%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -3,14 +3,14 @@ public:
     int countValidSelections(vector<int>& nums) {
         
         int total = accumulate(nums.begin(), nums.end(), 0LL), ls = 0, cases = 0;
+
         for (int num : nums) {
             int rs = total - ls - num;
             if (num == 0) {
-                if (ls == rs) {
+                if (ls == rs)
                     cases += 2;
-                } else if (ls == rs - 1 || ls - 1 == rs) {
+                else if (abs(ls - rs) == 1)
                     cases++;
-                }
             }
             ls += num;
         }
