319ca8948cc42237e1a448121194e0b712d26fc3
Sandipan pal <palsandipan68@gmail.com>
2025-10-31 10:13:56 +0530

    Time: 4 ms (26.50%) | Memory: 25.4 MB (67.30%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        
+        vector<int> ans;
+        bitset<101> vis;
+        if(nums.size() == 2)
+            return ans;
+        
+        for(int &ele: nums){
+
+            if(vis[ele])
+                ans.push_back(ele);
+            
+            vis[ele] = 1;
+        }
+
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
