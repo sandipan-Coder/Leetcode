4220083215cef8027f9f1ac62390b2570b80352c
Sandipan pal <palsandipan68@gmail.com>
2025-11-05 12:34:09 +0530

    Time: 779 ms (40.38%) | Memory: 280.7 MB (84.62%) - LeetSync

--- 3592-find-x-sum-of-all-k-long-subarrays-ii/find-x-sum-of-all-k-long-subarrays-ii.cpp
+++ 3592-find-x-sum-of-all-k-long-subarrays-ii/find-x-sum-of-all-k-long-subarrays-ii.cpp
@@ -1,73 +1,82 @@
 class Solution {
-public:
+    long long sum;
+    set<pair<int, int>> main;
+    set<pair<int, int>> secondary;
+private:
 
+    void insertAtSet(const pair<int, int> &node, int x){
+
+        if(main.size() < x || node > *main.begin()){
+
+            sum += (1LL * node.first * node.second);
+            main.insert(node);
+
+            if(main.size() > x){
+
+                auto smallest = *main.begin();
+                sum -= (1LL * smallest.first * smallest.second);
+                main.erase(smallest);
+                secondary.insert(smallest);
+            }
+        }
+        else
+            secondary.insert(node); 
+    }
+
+    void removeFromSet(const pair<int, int>& node, int x){
+
+        if(main.count(node)){
+
+            sum -= (1LL * node.first * node.second);
+            main.erase(node);
+
+            if((main.size() < x) && (secondary.size() != 0)){
+
+                auto largest = *secondary.rbegin();;
+                sum += (1LL * largest.first * largest.second);
+                main.insert(largest);
+                secondary.erase(largest);
+            }
+        }
+        else
+            secondary.erase(node);
+    }
+
+public:
     vector<long long> findXSum(vector<int>& nums, int k, int x) {
         
-        map<int, int> freq;
-        multiset<pair<int, int>> top, rest;
-        long long top_sum = 0;
+        sum = 0;
+        main.clear();
+        secondary.clear();
+        int n = nums.size();
+        unordered_map<int, int> mp;
         vector<long long> ans;
 
-        auto balance = [&]() {
-            while ((int)top.size() < x && !rest.empty()) {
-                auto it = prev(rest.end());
-                top.insert(*it);
-                top_sum += 1LL * it->first * it->second;
-                rest.erase(it);
-            }
-            while ((int)top.size() > x) {
-                auto it = top.begin();
-                top_sum -= 1LL * it->first * it->second;
-                rest.insert(*it);
-                top.erase(it);
-            }
-            while (!rest.empty() && !top.empty() && *prev(rest.end()) > *top.begin()) {
-                auto it1 = prev(rest.end());
-                auto it2 = top.begin();
-                top_sum += 1LL * it1->first * it1->second - 1LL * it2->first * it2->second;
-                rest.insert(*it2);
-                top.insert(*it1);
-                rest.erase(it1);
-                top.erase(it2);
-            }
-        };
+        int i = 0;
+        int j = 0;
 
-        auto add = [&](int num) {
-            pair<int, int> old = {freq[num], num};
-            if (top.find(old) != top.end()) {
-                top.erase(top.find(old));
-                top_sum -= 1LL * old.first * old.second;
-            } else if (rest.find(old) != rest.end()) {
-                rest.erase(rest.find(old));
+        while(j < n){
+
+            if(mp.count(nums[j]))
+                removeFromSet({mp[nums[j]], nums[j]}, x);
+            
+            mp[nums[j]]++;
+            insertAtSet({mp[nums[j]], nums[j]}, x);
+            
+            if(j - i + 1 == k){
+                ans.push_back(sum);
+
+                removeFromSet({mp[nums[i]], nums[i]}, x);
+                mp[nums[i]]--;
+                if(mp[nums[i]] == 0)
+                    mp.erase(nums[i]);
+                else
+                   insertAtSet({mp[nums[i]], nums[i]}, x); 
+                
+                i++;
             }
-            freq[num]++;
-            rest.insert({freq[num], num});
-            balance();
-        };
 
-        auto remove = [&](int num) {
-            pair<int, int> old = {freq[num], num};
-            if (top.find(old) != top.end()) {
-                top.erase(top.find(old));
-                top_sum -= 1LL * old.first * old.second;
-            } else {
-                rest.erase(rest.find(old));
-            }
-            freq[num]--;
-            if (freq[num] > 0)
-                rest.insert({freq[num], num});
-            else
-                freq.erase(num);
-            balance();
-        };
-
-        for (int i = 0; i < k; ++i) add(nums[i]);
-        ans.push_back(top_sum);
-
-        for (int i = k; i < (int)nums.size(); ++i) {
-            remove(nums[i - k]);
-            add(nums[i]);
-            ans.push_back(top_sum);
+            j++;
         }
 
         return ans;
