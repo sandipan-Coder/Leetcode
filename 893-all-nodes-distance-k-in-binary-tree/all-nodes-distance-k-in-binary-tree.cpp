f6222bb5dd3259cfb723d999a776564dcbb65598
Sandipan pal <palsandipan68@gmail.com>
2025-11-08 12:19:29 +0530

    Time: 3 ms (82.39%) | Memory: 15.6 MB (32.93%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,85 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+
+    void makeParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parentOfNodes){
+
+        queue<TreeNode*> nodes;
+        nodes.push(root);
+
+        while(!nodes.empty()){
+            TreeNode* curr = nodes.front();
+            nodes.pop();
+
+            if(curr->left){
+                parentOfNodes[curr->left] = curr;
+                nodes.push(curr->left);
+            }
+
+            if(curr->right){
+                parentOfNodes[curr->right] = curr;
+                nodes.push(curr->right);
+            }
+        }
+
+    }
+
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        
+        unordered_map<TreeNode*, TreeNode*> parentOfNodes;
+        makeParents(root, parentOfNodes);
+
+        unordered_map<TreeNode*, bool> visited;
+        queue<TreeNode*> q;
+        vector<int> ans;
+
+        q.push(target);
+        visited[target] = true;
+        int dist = -1;
+
+        while(!q.empty()){
+
+            int size = q.size();
+            dist++;
+            if(dist == k)
+                break;
+
+            for(int i = 0; i < size; i++){
+
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left && !visited[node->left]){
+                    q.push(node->left);
+                    visited[node->left] = true;
+                }
+
+                if(node->right && !visited[node->right]){
+                    q.push(node->right);
+                    visited[node->right] = true;
+                }
+
+                if(parentOfNodes[node] && !visited[parentOfNodes[node]]){
+                    q.push(parentOfNodes[node]);
+                    visited[parentOfNodes[node]] = true;
+                }
+            }
+        }
+
+        while(!q.empty()){
+            TreeNode* curr = q.front();
+            q.pop();
+            ans.push_back(curr->val);
+        }
+
+        return ans;
+    }
+};
