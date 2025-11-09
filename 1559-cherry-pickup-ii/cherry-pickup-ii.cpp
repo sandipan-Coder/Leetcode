85c4aafcd1f21924c420a075cb7d1c29417ce637
Sandipan pal <palsandipan68@gmail.com>
2025-11-09 10:35:35 +0530

    Time: 51 ms (52.20%) | Memory: 13.8 MB (87.38%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -64,6 +64,52 @@ private:
         return dp[row][col1][col2] = maxi;
     }
 
+    // Tabulation
+    int solveTab(vector<vector<int>>& grid){
+
+        // Base Case
+        for(int j1 = 0; j1 < m; j1++){
+            for(int j2 = 0; j2 < m; j2++){
+                
+                if(j1 == j2)
+                    dp[n-1][j1][j2] = grid[n-1][j1];
+                else
+                    dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+
+        // Explore all paths
+        for(int row = n-2; row >= 0; row--){
+            for(int col1 = 0; col1 < m; col1++){
+                for(int col2 = 0; col2 < m; col2++){
+
+                    int maxi = -1e8;
+                    // Explore all paths
+                    for(int j1 = -1; j1 <= 1; j1++){
+                        for(int j2 = -1; j2 <= 1; j2++){
+
+                            int value = 0;
+                            if(col1 == col2)
+                                value = grid[row][col1];
+                            else
+                                value = grid[row][col1] + grid[row][col2];
+                            if(col1+j1 >= 0 && col1+j1 < m && col2+j2 >= 0 && col2+j2 < m)
+                                value += dp[row+1][col1+j1][col2+j2];
+                            else
+                                value += -1e8;
+                            
+                            maxi = max(maxi, value);
+                        }
+                    }
+
+                    dp[row][col1][col2] = maxi;
+                }
+            }
+        }
+
+        return dp[0][0][m - 1];
+    }
+
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         
@@ -74,7 +120,13 @@ public:
         // return solve(0, 0, m-1, grid);
 
         // Recursive + Memoization
+        /*
         memset(dp, -1, sizeof(dp));
         return solveMem(0, 0, m-1, grid);
+        */
+
+        // Tabulation
+        memset(dp, 0, sizeof(dp));
+        return solveTab(grid);
     }
 };
