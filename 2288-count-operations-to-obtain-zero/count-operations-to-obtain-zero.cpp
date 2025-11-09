fdcd7752f0bcbf41ddc1fbd4a1f2c520acbfbe22
Sandipan pal <palsandipan68@gmail.com>
2025-11-09 09:55:43 +0530

    Time: 2 ms (20.24%) | Memory: 8 MB (40.22%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -5,11 +5,14 @@ public:
         int count = 0;
 
         while(num1 && num2){
-            if(num1 >= num2)
-                num1 -= num2;
-            else
-                num2 -= num1;
-            count++;
+            if(num1 >= num2){
+                count += (num1 / num2);
+                num1 %= num2;
+            }
+            else{
+                count += (num2 / num1);
+                num2 %= num1;
+            }
         }
 
         return count;
