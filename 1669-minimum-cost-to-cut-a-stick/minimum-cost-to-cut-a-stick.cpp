fc8ad98e089a741e3f4c8f98ad67a53a793b0a8f
Sandipan pal <palsandipan68@gmail.com>
2025-11-15 12:21:59 +0530

    Time: 28 ms (56.56%) | Memory: 11.2 MB (96.74%) - LeetSync

---
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,62 @@
+class Solution {
+    int dp[102][102];
+private:
+
+    // Recursive
+    int solve(int i, int j, vector<int>& cuts){
+
+        if(i > j)
+            return 0;
+        
+        int mini = INT_MAX;
+
+        for(int idx = i; idx <= j; idx++){
+
+            int cost = cuts[j + 1] - cuts[i - 1] + solve(i, idx - 1, cuts) +
+                        solve(idx + 1, j, cuts);
+                    
+            mini = min(mini, cost);
+        }
+
+        return mini;
+    }
+
+    // Recursive + Memoization
+    int solveMem(int i, int j, vector<int>& cuts){
+
+        if(i > j)
+            return 0;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        int mini = INT_MAX;
+
+        for(int idx = i; idx <= j; idx++){
+
+            int cost = cuts[j + 1] - cuts[i - 1] + solveMem(i, idx - 1, cuts) +
+                        solveMem(idx + 1, j, cuts);
+                    
+            mini = min(mini, cost);
+        }
+
+        return dp[i][j] = mini;
+    }
+
+public:
+    int minCost(int n, vector<int>& cuts) {
+
+        int len = cuts.size();
+        cuts.insert(cuts.begin(), 0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+
+        // Recursive
+        // return solve(1, len, cuts);
+
+        // Recursive + Memoization
+        memset(dp, -1, sizeof(dp));
+        return solveMem(1, len, cuts);
+
+    }
+};
