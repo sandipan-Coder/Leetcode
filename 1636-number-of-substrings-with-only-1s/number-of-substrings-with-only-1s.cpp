4197daea71c8df1192d2a54afdd6e5ebb5d9425f
Sandipan pal <palsandipan68@gmail.com>
2025-11-16 10:20:14 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (39.33%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -7,21 +7,22 @@ public:
         int MOD = 1e9 + 7;
 
         int idx = 0;
+        int count = 0;
 
         while(idx < n){
 
             if(s[idx] == '1'){
-                int count = 0;
 
                 while(idx < n && s[idx] == '1'){
                     count++;
+                    ans = (ans + count) % MOD;
                     idx++;
                 }
-
-                int cal = ((1LL * count * ((count + 1) % MOD)) % MOD / 2) % MOD;
-                ans = (ans + cal) % MOD;
             }
-            idx++;
+            else{
+                count = 0;
+                idx++;
+            }
         }
 
         return ans;
