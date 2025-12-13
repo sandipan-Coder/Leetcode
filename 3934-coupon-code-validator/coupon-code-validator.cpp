e7c23ef3c464eabc914586455c46e6859b2529bf
Sandipan pal <palsandipan68@gmail.com>
2025-12-13 09:36:57 +0530

    Time: 12 ms (22.75%) | Memory: 69.4 MB (56.22%) - LeetSync

--- 3934-coupon-code-validator/coupon-code-validator.cpp
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -1,33 +1,40 @@
 class Solution {
-private:
-
-    bool isValidBusiness(string str){
-        return (str == "electronics" || str == "grocery" || str == "pharmacy" || str == "restaurant");
-    }
-
-    bool isValidCode(string str){
-        return regex_match(str, regex("[A-Za-z0-9_]+"));
-    }
-
 public:
     vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
-        
         int n = code.size();
-        map<string, multiset<string>> mp;
         vector<string> ans;
+        vector<string> categ={"electronics", "grocery", "pharmacy", "restaurant"};
+        unordered_map<string,vector<string>> mp;
 
         for(int i = 0; i < n; i++){
-            if(isActive[i] && isValidBusiness(businessLine[i]) && (code[i].size() != 0)){
-                if(isValidCode(code[i]))
-                    mp[businessLine[i]].insert(code[i]);
+            if(!isActive[i])
+                continue;
+
+            if(businessLine[i] != categ[0] && businessLine[i] != categ[1] && businessLine[i] != categ[2] && businessLine[i] != categ[3])
+                continue;
+            int f = 1;
+            if(code[i].size() == 0)
+                continue;
+            for(int j = 0; j < code[i].size(); j++){
+                if(!((code[i][j] == '_' ) || (code[i][j]-'a' < 26 &&code[i][j]-'a' >= 0) || (code[i][j]-'A' < 26 && code[i][j]-'A' >= 0) || (code[i][j]-'0' <= 9&&code[i][j]-'0' >= 0))){
+                    f = -1;
+                    break;
+                }
             }
+            if(f == -1)
+                continue;
+            mp[businessLine[i]].push_back(code[i]);
         }
 
-        for(auto &it: mp){
-            for(auto ele: it.second)
-                ans.push_back(ele);
+        for(int i = 0; i < 4; i++){
+            auto it = mp.find(categ[i]);
+            if(it == mp.end())
+                continue;
+            sort(it->second.begin(),it->second.end());
+            for(int j = 0; j < it->second.size(); j++)
+                ans.push_back(it->second[j]);
         }
-
+        
         return ans;
     }
 };
