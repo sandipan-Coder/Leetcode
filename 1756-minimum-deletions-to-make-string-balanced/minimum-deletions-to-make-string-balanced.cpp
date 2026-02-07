100c12e9e93191bade254c46417c1a53546b624f
Sandipan pal <palsandipan68@gmail.com>
2026-02-07 10:12:24 +0530

    Time: 74 ms (7.99%) | Memory: 55.3 MB (10.50%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -3,23 +3,28 @@ public:
     int minimumDeletions(string s) {
         
         int n = s.length();
-        stack<char> st;
-        int count = 0;
+        vector<int> beforeB(n, 0);
+        vector<int> afterA(n, 0);
+        int count = INT_MAX;
+
+        for(int i = 1; i < n; i++) {
+
+            if(s[i - 1] == 'b')
+                beforeB[i]++;
+            if(s[n - i] == 'a')
+                afterA[n - i - 1]++;
+            
+            afterA[n - i - 1] += afterA[n - i];
+            beforeB[i] += beforeB[i - 1];
+        }
 
         for(int i = 0; i < n; i++) {
 
-            char ch = s[i];
-
-            if(st.empty())
-                st.push(ch);
-            else if((ch == st.top()) || (st.top() == 'a' && ch == 'b'))
-                st.push(ch);
-            else {
-                st.pop();
-                count++;
-            }   
+            int del = beforeB[i] + afterA[i];
+            count = min(count, del);
         }
 
+        
         return count;
     }
 };
