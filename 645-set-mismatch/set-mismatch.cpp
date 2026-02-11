8a49cf88d4d6d9d86578c656dd7380530828e9ba
Sandipan pal <palsandipan68@gmail.com>
2026-02-11 10:04:13 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (64.23%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        
+        int n = nums.size();
+        int dup = -1;
+        int miss = -1;
+
+        for(int i = 0; i < n; i++){
+
+            if(nums[abs(nums[i]) - 1] < 0)
+                dup = abs(nums[i]);
+            else
+                nums[abs(nums[i]) - 1] *= (-1);
+        }
+
+        for(int i = 0; i < n; i++){
+
+            if(nums[i] > 0) {
+                miss = (i + 1);
+                break;
+            }
+        }
+
+        return {dup, miss};
+    }
+};
