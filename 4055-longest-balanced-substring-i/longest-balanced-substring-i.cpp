f3a0e1768f32dcc04b57c5bddca5c599479cb3b4
Sandipan pal <palsandipan68@gmail.com>
2026-02-12 08:56:00 +0530

    Time: 262 ms (18.50%) | Memory: 25.5 MB (52.24%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+
+    bool isBallanced(vector<int> &freq) {
+
+        int count = 0;
+
+        for(int i = 0; i < 26; i++) {
+
+            if((count == 0) && (freq[i] > 0))
+                count = freq[i];
+            else if((count != 0) && (freq[i] > 0) && (count != freq[i]))
+                return false;
+        }
+
+        return true;
+    }
+
+public:
+    int longestBalanced(string s) {
+        
+        int n = s.length();
+        int maxLen = 1;
+
+        for(int i = 0; i < n; i++) {
+            vector<int> freq(26, 0);
+            for(int j = i; j < n; j++) {
+                
+                freq[s[j] - 'a']++;
+                if(isBallanced(freq))
+                    maxLen = max(maxLen, (j - i + 1));
+            }
+        }
+
+        return maxLen;
+    }
+};
