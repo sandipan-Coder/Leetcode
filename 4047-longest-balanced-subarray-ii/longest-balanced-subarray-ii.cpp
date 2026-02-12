ebcd6382dced981a353874a1919ab62351d72fe6
Sandipan pal <palsandipan68@gmail.com>
2026-02-12 11:26:21 +0530

    Time: 607 ms (47.92%) | Memory: 160.3 MB (58.11%) - LeetSync

--- 4047-longest-balanced-subarray-ii/longest-balanced-subarray-ii.cpp
+++ 4047-longest-balanced-subarray-ii/longest-balanced-subarray-ii.cpp
@@ -1,104 +1,162 @@
-struct SegTree {
-    struct Node {
-        int mn = 0, mx = 0, lazy = 0;
-    };
+class Solution {
 
+    vector<int> segMin, segMax, lazy;
     int n;
-    vector<Node> st;
 
-    SegTree(int n_) : n(n_), st(4 * n_) {}
+private:
 
-    // add a value v to the node p
-    void apply(int p, int v) {
-        st[p].mn += v;
-        st[p].mx += v;
-        st[p].lazy += v;
-    }
+    void propagate(int i, int l, int r) {
+        if (lazy[i] != 0) {
+            segMin[i] += lazy[i];
+            segMax[i] += lazy[i];
 
-    // to push the lazy value to the childrens else values get outdated
-    void push(int p) {
-        if (st[p].lazy != 0) {
-            apply(p << 1, st[p].lazy);
-            apply(p << 1 | 1, st[p].lazy);
-            st[p].lazy = 0;
+            if (l != r) {
+                lazy[2*i+1] += lazy[i];
+                lazy[2*i+2] += lazy[i];
+            }
+            lazy[i] = 0;
         }
     }
 
-    // used by parent to update its mn and mx from childrens
-    void pull(int p) {
-        st[p].mn = min(st[p << 1].mn, st[p << 1 | 1].mn);
-        st[p].mx = max(st[p << 1].mx, st[p << 1 | 1].mx);
-    }
+    // Range add val to [start, end]
+    void updateRange(int start, int end, int i, int l, int r, int val) {
+        propagate(i, l, r); //make sure to propagate before hand
 
-    // updating the ranges
-    void rangeAdd(int p, int l, int r, int ql, int qr, int v) {
-        if (qr < l || r < ql) return;
-        if (ql <= l && r <= qr) {
-            apply(p, v);
+        if (l > end || r < start) return;
+
+        //[start...end[ is fully inside range of current node [l..r]
+        if (l >= start && r <= end) {
+            lazy[i] += val;
+            propagate(i, l, r);
             return;
         }
-        push(p);
-        int m = (l + r) >> 1;
-        rangeAdd(p << 1, l, m, ql, qr, v);
-        rangeAdd(p << 1 | 1, m + 1, r, ql, qr, v);
-        pull(p);
+
+        int mid = (l + r) / 2;
+        updateRange(start, end, 2*i+1, l, mid, val);
+        updateRange(start, end, 2*i+2, mid+1, r, val);
+
+        segMin[i] = min(segMin[2*i+1], segMin[2*i+2]);
+        segMax[i] = max(segMax[2*i+1], segMax[2*i+2]);
     }
 
-    void rangeAdd(int l, int r, int v) {
-        if (l > r) return;
-        rangeAdd(1, 0, n - 1, l, r, v);
-    }
+    int findLeftMostZero(int i, int l, int r) {
+        propagate(i, l, r);
 
-    // Find leftmost index in [ql..qr] where value == 0, else -1
-    int leftmostZero(int p, int l, int r, int ql, int qr) {
-        if (qr < l || r < ql) return -1;
-        if (st[p].mn > 0 || st[p].mx < 0) return -1; // can't contain 0
-        if (l == r) return l;
-
-        push(p);
-        int m = (l + r) >> 1;
-        int left = leftmostZero(p << 1, l, m, ql, qr);
-        if (left != -1) return left;
-        return leftmostZero(p << 1 | 1, m + 1, r, ql, qr);
-    }
-
-    int leftmostZero(int ql, int qr) {
-        if (ql > qr) return -1;
-        return leftmostZero(1, 0, n - 1, ql, qr);
-    }
-};
-
-
-
-class Solution {
-public:
-    int longestBalanced(vector<int>& nums) {
-        
-
-        int n = (int)nums.size();
-
-        SegTree st(n);                 // A[l] initially 0 for all l (when r = -1)
-        unordered_map<int,int> last;   // last occurrence index
-        last.reserve(n * 2);
-
-        int ans = 0;
-
-        for (int r = 0; r < n; r++) {
-            int x = nums[r];
-            int prev = -1;
-
-            auto it = last.find(x);
-            if (it != last.end()) prev = it->second;
-
-            int delta = (x & 1) ? +1 : -1;   // odd:+1, even:-1 for A = odd-even
-
-            st.rangeAdd(prev + 1, r, delta); // x becomes newly distinct for these l
-            last[x] = r;
-
-            int l0 = st.leftmostZero(0, r);  // earliest l with A[l] == 0
-            if (l0 != -1) ans = max(ans, r - l0 + 1);
+        if(segMin[i] > 0 || segMax[i] < 0) {
+            return -1;
         }
 
-        return ans;
+        if(l == r) {
+            return l;
+        }
+
+        int mid = l + (r-l)/2;
+        int leftResult = findLeftMostZero(2*i+1, l, mid);
+        if(leftResult != -1)
+            return leftResult;
+        
+        return findLeftMostZero(2*i+2, mid+1, r);
     }
+
+
+public:
+
+    // *************************  //Approach-1 (Brute Force) - TLE *******************
+    //T.C : O(n^2)
+    //S.C : O(n)
+    /*
+    int longestBalanced(vector<int>& nums) {
+        
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        vector<int> cumSum(n, 0);
+        int maxLen = 0;
+
+        // For even = +1, odd = -1.
+        for(int i = 0; i < n; i++) {
+
+            int val = (nums[i] % 2) ? -1 : 1;
+
+            int prevIdx = -1;
+            // We will try to find if the current element is previously seen or not.
+            if(mp.count(nums[i]))  
+                prevIdx = mp[nums[i]];
+            
+
+            // If we seen the element before then .......
+            // [0 .... prevIdx] add (-val).
+            if(prevIdx != -1) {
+                for(int j = 0; j <= prevIdx; j++) // Renge Query Update -> Segment Tree log(n).
+                    cumSum[j] -= val;
+            }
+
+            // [0 .... i] add (+val).
+            for(int j = 0; j <= i; j++) // Renge Query Update -> Segment Tree log(n).
+                cumSum[j] += val;
+
+            
+            // Try to find leftmost 0 of the cumulative sum array.
+            for(int j = 0; j <= i; j++) {
+                if(cumSum[j] == 0) {  // Renge Search -> Segment Tree log(n).
+                    maxLen = max(maxLen, (i - j + 1));
+                    break;
+                }
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return maxLen;
+    }
+
+    */
+
+    //******************** Approach - 2 (Using Segment Tree RMin Max Query ************
+    //T.C : O(nlogn)
+    //S.C : O(n)    
+
+    int longestBalanced(vector<int>& nums) {
+        
+        n = nums.size();
+        segMin.assign(4*n, 0);
+        segMax.assign(4*n, 0);
+        lazy.assign(4*n, 0);
+
+        unordered_map<int, int> mp;
+        int maxLen = 0;
+
+        // For even = +1, odd = -1.
+        for(int i = 0; i < n; i++) {
+
+            int val = (nums[i] % 2) ? -1 : 1;
+
+            int prevIdx = -1;
+            // We will try to find if the current element is previously seen or not.
+            if(mp.count(nums[i]))  
+                prevIdx = mp[nums[i]];
+            
+
+            // If we seen the element before then .......
+            // [0 .... prevIdx] add (-val).
+            if(prevIdx != -1) {
+                // Renge Query Update -> Segment Tree log(n).
+                updateRange(0, prevIdx, 0, 0, n-1, -val);
+            }
+
+            // [0 .... i] add (+val).
+            // Renge Query Update -> Segment Tree log(n).
+            updateRange(0, i, 0, 0, n-1, val);    
+
+            
+            // Try to find leftmost 0 of the cumulative sum array.
+            int l = findLeftMostZero(0, 0, n-1);
+            if(l != -1)
+                maxLen = max(maxLen, (i - l + 1));
+
+            mp[nums[i]] = i;
+        }
+
+        return maxLen;
+    }
+
 };
