c56af066c16d81d2017d41b50307a2dfbaa834b2
Sandipan pal <palsandipan68@gmail.com>
2026-02-16 09:38:09 +0530

    Time: 4 ms (29.28%) | Memory: 8.3 MB (15.72%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -7,8 +7,10 @@ public:
         for(int i = 0; i < 32; i++) {
 
             ans <<= 1;
-            ans |= (n & 1);
-            n >>= 1;
+            // ans |= (n & 1);
+            // n >>= 1;
+
+            ans |= (n >> i) & 1;
         }
         
         return ans;
