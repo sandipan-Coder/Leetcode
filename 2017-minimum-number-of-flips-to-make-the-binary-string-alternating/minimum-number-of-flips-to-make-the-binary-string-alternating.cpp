afec4dd4353d87cba9b0786c335ec15d14169c02
Sandipan pal <palsandipan68@gmail.com>
2026-03-07 11:10:28 +0530

    Time: 19 ms (57.20%) | Memory: 15.6 MB (70.00%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -107,18 +107,79 @@ private:
         return ans;
     }
 
+    // Without using extra space
+    int approach3(string s) {
+            
+        int n = s.length();
+        int ans = INT_MAX;
+
+        // We know that the answer may be in this two type
+        // S1 :- "010101"
+        // S2 :- "101010"
+
+        // From the above reprasentation we understand that
+        // s1 -> evenIdx = '0' , oddIdx = '1'
+        // s2 -> evenIdx = '1', oddIdx = '0'
+
+        int flip1 = 0;
+        int flip2 = 0;
+        int i = 0, j = 0;
+
+        while(j < (2 * n)) {
+            
+            char desireStr1 = (j % 2) ? '1' : '0';
+            char desireStr2 = (j % 2) ? '0' : '1';
+
+            // If bit of s[j] not match with str1[j] count of flip1 increase.
+            if(s[j % n] != desireStr1)
+                flip1++;
+
+            // If bit of s[j] not match with str2[j] count of flip2 increase.
+            if(s[j % n] != desireStr2)
+                flip2++;
+            
+            // If the window size is bigger then the size of the string.
+            if((j - i + 1 ) > n) {
+
+                char desireStr11 = (i % 2) ? '1' : '0';
+                char desireStr22 = (i % 2) ? '0' : '1';
+
+                if(s[i % n] != desireStr11)
+                    flip1--;
+                
+                if(s[i % n] != desireStr22)
+                    flip2--;
+                
+                i++;
+            }
+
+            if((j - i + 1 ) == n) 
+                ans = min({ans, flip1, flip2});
+
+            j++;
+        }
+
+        return ans;
+    }
+
 public:
     int minFlips(string s) {
         
         // Approach 1
         // Time :- O(N).
         // Space :- O(2 * N).
-
         /*
         return approach1(s);
         */
 
+        // Time :- O(N).
+        // Space :- O(2 * N).
+        /*
         return approach2(s);
+        */
 
+        // Time :- O(N).
+        // Space :- O(1).
+        return approach3(s);
     }
 };
