6f03b9ae36bfe000a9d62d7c02cb4a087ae5a2c6
Sandipan pal <palsandipan68@gmail.com>
2026-03-08 10:11:44 +0530

    Time: 11 ms (13.72%) | Memory: 38.2 MB (11.69%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,59 @@
+class Solution {
+private:
+
+    int convert_number(string str) {
+
+        int len = str.length();
+        int num = 0;
+
+        for(int i = len - 1; i >= 0; i--) {
+
+            int bit = (str[i] == '0') ? 0 : 1;
+            num = (num * 2) + bit;
+        
+        }
+        
+        return num;
+    }
+
+    string stringConversion(int num, int &n) {
+
+        string ans = "";
+
+        while(num) {
+
+            int rem = num % 2;
+            ans += to_string(rem);
+            num /= 2;
+        }
+
+        while(ans.size() != n)
+            ans += '0';
+        
+        // reverse(ans.begin(), ans.end());
+    
+        return ans;
+    }
+
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        
+        int n = nums.size();
+        int no_of_element = (2 << n);
+        vector<int> elements(no_of_element, 0);
+
+        for(string str: nums) {
+
+            int num = convert_number(str);
+            elements[num]++;
+        }
+
+        for(int i = 0; i < no_of_element; i++) {
+
+            if(elements[i] == 0)
+                return stringConversion(i, n);
+        }
+
+        return "-1";
+    }
+};
