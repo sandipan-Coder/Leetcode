878f060d69b6472e619d057d03b79868a4062101
Sandipan pal <palsandipan68@gmail.com>
2026-03-10 10:21:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (53.88%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -15,10 +15,26 @@ private:
         return dp[steps] = (solve(steps + 1, n) + solve(steps + 2, n));
     }
 
+    int solveBottom(int n) {
+
+        int dp1[48];
+        memset(dp1, 0, sizeof(dp1));
+        dp1[n] = 1;
+
+        for(int i = n-1; i >= 0; i--)
+            dp1[i] = dp1[i + 1] + dp1[i + 2];
+        
+        return dp1[0];
+    }
+
 public:
     int climbStairs(int n) {
         
+        /*
         memset(dp, -1, sizeof(dp));
         return solve(0, n);
+        */
+
+        return solveBottom(n);
     }
 };
