7871931edecf7ab89bdbca56102229d9e712d640
Sandipan pal <palsandipan68@gmail.com>
2026-03-14 08:52:10 +0530

    Time: 6 ms (51.12%) | Memory: 7.8 MB (89.21%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
 
-    void solve(int n, string &curr, vector<string> &ans) {
+    void solve1(int n, string &curr, vector<string> &ans) {
 
         if(curr.length() == n) {
             ans.push_back(curr);
@@ -14,23 +14,71 @@ private:
                 continue;
             
             curr.push_back(ch);
-            solve(n, curr, ans);
+            solve1(n, curr, ans);
 
             curr.pop_back();
         }
     }
 
-public:
-    string getHappyString(int n, int k) {
+    string approach1(int n, int k) {
         
         string curr = "";
         vector<string> ans;
 
-        solve(n, curr, ans);
+        solve1(n, curr, ans);
 
         if(k > ans.size())
             return "";
 
         return ans[k - 1];
+    }
+
+    void solve2(int n, string &curr, string &ans, int &count, int k) {
+
+        if(curr.length() == n) {
+            
+            count++;
+            if(count == k)
+                ans = curr;
+
+            return;
+        }
+
+        for(char ch = 'a'; ch <= 'c'; ch++) {
+
+            if(!curr.empty() && ch == curr.back())
+                continue;
+            
+            curr.push_back(ch);
+            solve2(n, curr, ans, count, k);
+
+            curr.pop_back();
+        }
+    }
+
+    string approach2(int n, int k) {
+        
+        string curr = "";
+        string ans = "";
+        int count = 0;
+
+        solve2(n, curr, ans, count, k);
+
+        return ans;
+    }
+
+public:
+    string getHappyString(int n, int k) {
+        
+       // Time :- O(N * 2^N).
+       // Space :- O(2^N).
+
+       //return approach1(n, k);
+
+       // Optimize space
+       // Time :- O(N * 2^N).
+       // Space :- O(1).
+       return approach2(n, k);
+
     }
 };
