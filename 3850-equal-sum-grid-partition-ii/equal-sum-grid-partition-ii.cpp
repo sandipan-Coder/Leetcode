bc866399d68b8fb1e2cfa8b0d28d7150dd7af498
Sandipan pal <palsandipan68@gmail.com>
2026-03-26 14:01:35 +0530

    Time: 869 ms (61.54%) | Memory: 390.4 MB (58.46%) - LeetSync

--- 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.cpp
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.cpp
@@ -24,13 +24,14 @@ private:
 
             if(diff == 0)
                 return true;
-            if(diff == (ll)arr[0][0])
+            if(diff == (ll)arr[0][0])  // If first element of first row is same as diff
                 return true;
-            if(diff == (ll)arr[0][m - 1])
+            if(diff == (ll)arr[0][m - 1])  // If last element of first row is same as diff
                 return true;
-            if(diff == (ll)arr[i][0])
+            if(diff == (ll)arr[i][0])  // This is for single column array.
                 return true;
             
+            // If cutting index is more then 0 and no. of column greater than 1 then we can remove any element from the array.
             if(i > 0 && m > 1 && st.count(diff))
                 return true;
         }
@@ -43,6 +44,7 @@ public:
         
         int n = grid.size();
         int m = grid[0].size();
+        // For vertical cut we check it using horizontal cut function just taking the transpose of the given matrix.
         vector<vector<int>> transposeMatrix(m, vector<int>(n, 0));
 
         for(int i = 0; i < n; i++) {
@@ -53,18 +55,20 @@ public:
             }
         }
 
+        // Horizontal Cut checking.
         if(check_Horizontal_Cut(grid))
             return true;
         
-        reverse(begin(grid), end(grid));
+        reverse(begin(grid), end(grid));  // This is for both side checking
 
         if(check_Horizontal_Cut(grid))
             return true;
 
+        // Vertical cut checking
         if(check_Horizontal_Cut(transposeMatrix))
             return true;
 
-        reverse(begin(transposeMatrix), end(transposeMatrix));
+        reverse(begin(transposeMatrix), end(transposeMatrix));  // This is for both side checking
 
         if(check_Horizontal_Cut(transposeMatrix))
             return true;
