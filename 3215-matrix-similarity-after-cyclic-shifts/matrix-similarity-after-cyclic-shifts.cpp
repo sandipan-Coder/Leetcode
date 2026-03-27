051e99f2cbfabb724ce08ad32f41c97267e5ee87
Sandipan pal <palsandipan68@gmail.com>
2026-03-27 08:56:15 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (49.69%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -1,44 +1,18 @@
 class Solution {
-private:
-
-    void left_shift(vector<int> &arr, int k){
-
-        reverse(arr.begin(), arr.begin() + k);
-        reverse(arr.begin() + k, arr.end());
-        reverse(arr.begin(), arr.end());
-
-        return;
-    }
-
-    void right_shift(vector<int> &arr, int k){
-
-        reverse(arr.begin(), arr.end());
-        reverse(arr.begin(), arr.begin() + k);
-        reverse(arr.begin() + k, arr.end());
-
-        return;
-    }
-
 public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
         
         int n = mat.size();
         int m = mat[0].size();
-        vector<vector<int>> grid = mat;
-        int count = k;
-
-        count = (count % m);
-        if(count == 0)
-            return true;
 
         for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
 
-            if(i % 2) 
-                right_shift(grid[i], count);
-            else
-                left_shift(grid[i], count);
+                if(mat[i][(j + k) % m] != mat[i][j])
+                    return false;
+            }
         }
 
-        return (grid == mat);
+        return true;
     }
 };
