92f10d39aded28576b8faf16832c0548988f15cb
Sandipan pal <palsandipan68@gmail.com>
2026-03-31 12:03:59 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (60.70%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -42,6 +42,25 @@ private:
         return dp[idx][isBuy] = profit;
     }
 
+    // Tabulation
+    int solveTab(vector<int>& prices) {
+
+        vector<vector<int>> dp(n+3, vector<int>(2, 0));
+
+        for(int idx = n-1; idx >= 0; idx--) {
+            for(int buy = 0; buy <= 1; buy++){
+
+                if(buy) {
+                    dp[idx][buy] = max((-prices[idx] + dp[idx+1][0]), (0 + dp[idx+1][1]));
+                } else {
+                    dp[idx][buy] = max((prices[idx] + dp[idx+2][1]), (0 + dp[idx+1][0]));
+                }
+            }
+        }       
+
+        return dp[0][1];
+    }
+
 public:
     int maxProfit(vector<int>& prices) {
         
@@ -53,7 +72,12 @@ public:
         */
 
         // Recursive + Memoization
+        /*
         vector<vector<int>> dp(n+3, vector<int>(2, -1));
         return solveMem(0, true, prices, dp);
+        */
+
+        // Tabulation
+        return solveTab(prices);
     }
 };
