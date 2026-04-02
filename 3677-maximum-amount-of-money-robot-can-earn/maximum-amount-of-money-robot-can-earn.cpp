823ef25c38b531c45adfcfd51eb4859d4a1f017d
Sandipan pal <palsandipan68@gmail.com>
2026-04-02 09:16:30 +0530

    Time: 682 ms (22.13%) | Memory: 275.8 MB (55.32%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -100,7 +100,54 @@ private:
     }
 
     // Top down
-   
+    int solveTab(vector<vector<int>>& coins) {
+        
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(3, INT_MIN)));
+        
+        for(int k = 0; k <= 2; k++) {
+            if(coins[n-1][m-1] < 0 && k > 0)
+                dp[n-1][m-1][k] = 0;
+            else
+                dp[n-1][m-1][k] = coins[n-1][m-1];
+        }
+
+        for(int i = n-1; i >= 0; i--){
+            for(int j = m-1; j >= 0; j--){
+
+                if(i == n-1 && j == m-1)
+                    continue;
+
+                for(int k = 0; k <= 2; k++){
+  
+                    int ans = INT_MIN;
+
+                    // Down Movement
+                    if(i + 1 < n) {
+                        
+                        if(dp[i+1][j][k] != INT_MIN)
+                            ans = max(ans, coins[i][j] + dp[i+1][j][k]);
+
+                        if(coins[i][j] < 0 && k > 0 && dp[i+1][j][k-1] != INT_MIN)
+                            ans = max(ans, dp[i+1][j][k-1]);
+                    }
+
+                    // Right Movement
+                    if(j + 1 < m) {
+                        
+                        if(dp[i][j+1][k] != INT_MIN)
+                            ans = max(ans, coins[i][j] + dp[i][j+1][k]);
+
+                        if(coins[i][j] < 0 && k > 0 && dp[i][j+1][k-1] != INT_MIN)
+                            ans = max(ans, dp[i][j+1][k-1]);
+                    }
+
+                    dp[i][j][k] = ans;
+                }
+            }
+        }
+
+        return dp[0][0][2];
+    }
 
 
 public:
@@ -115,7 +162,12 @@ public:
         */
 
         // Recursive + Memoization
+        /*
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(3, INT_MIN)));
         return solveMem(0, 0, 2, coins, dp);
+        */
+
+        // Tabulation
+        return solveTab(coins);
     }
 };
