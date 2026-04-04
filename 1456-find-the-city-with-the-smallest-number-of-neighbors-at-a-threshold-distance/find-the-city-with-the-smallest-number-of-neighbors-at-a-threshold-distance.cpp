8491d116ee724c6b9c98a3e9c0a86cc880c3a586
Sandipan pal <palsandipan68@gmail.com>
2026-04-04 12:17:06 +0530

    Time: 31 ms (13.64%) | Memory: 20.9 MB (11.74%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,41 +1,67 @@
 class Solution {
+private:
+
+    int dijkstra_algo(int src, vector<pair<int, int>> adj[], int &n, 
+            int &distanceThreshold) {
+
+        vector<int> dist(n, 1e9);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        pq.push({0, src});
+        dist[src] = 0;
+
+        while(!pq.empty()) {
+
+            auto it = pq.top();
+            pq.pop();
+
+            int cost = it.first;
+            int node = it.second;
+
+            for(auto nei: adj[node]) {
+                int neiNode = nei.first;
+                int wt = nei.second;
+
+                if(cost + wt < dist[neiNode]) {
+                    dist[neiNode] = cost + wt;
+                    pq.push({dist[neiNode], neiNode});
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+
+            if(i == src)
+                continue;
+
+            if(dist[i] <= distanceThreshold)
+                count++;
+        }
+
+        return count;
+    }
+
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        
-        vector<vector<int>> dist(n, vector<int>(n, 1e8));
 
-        for(int i = 0; i < n; i++)
-            dist[i][i] = 0;
-
+        vector<pair<int, int>> adj[n];
         for(auto it: edges) {
+
             int u = it[0];
             int v = it[1];
             int wt = it[2];
 
-            dist[u][v] = wt;
-            dist[v][u] = wt;
-        }
-
-        for(int k = 0; k < n; k++) {
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    
-                    if(dist[i][k] != 1e8 && dist[k][j] != 1e8)
-                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-                }
-            }
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
         }
 
         int countCity = n+1;
         int city = -1;
 
         for(int i = 0; i < n; i++) {
-            int count = 0;
-            for(int j = 0; j < n; j++) {
-
-                if(dist[i][j] <= distanceThreshold)
-                    count++;
-            }
+            
+            int count = dijkstra_algo(i, adj, n, distanceThreshold);
 
             if(count <= countCity) {
                 countCity = count;
