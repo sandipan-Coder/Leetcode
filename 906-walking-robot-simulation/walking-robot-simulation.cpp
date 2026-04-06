548332e7083d8f662753e562351003b0f91896a9
Sandipan pal <palsandipan68@gmail.com>
2026-04-06 09:57:28 +0530

    Time: 27 ms (53.33%) | Memory: 40.2 MB (29.33%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.cpp
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -6,9 +6,10 @@ struct pair_hash {
 
 
 class Solution {
-public:
-    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        
+private:
+
+    int approach1(vector<int>& commands, vector<vector<int>>& obstacles){
+
         int x = 0;
         int y = 0;
         char dir = 'N';
@@ -46,7 +47,7 @@ public:
             }
             else {
 
-                if(dir == 'N') {
+                if(dir == 0) {
                     for(int i = 1; i <= com; i++) {
                         y += 1;
 
@@ -57,7 +58,7 @@ public:
                         maxDis = max(maxDis, ((x * x) + (y * y)));
                     }
                 }
-                else if(dir == 'S') {
+                else if(dir == 2) {
                     for(int i = 1; i <= com; i++) {
                         y -= 1;
 
@@ -68,7 +69,7 @@ public:
                         maxDis = max(maxDis, ((x * x) + (y * y)));
                     }
                 }
-                else if(dir == 'E') {
+                else if(dir == 1) {
                     for(int i = 1; i <= com; i++) {
                         x += 1;    
 
@@ -94,5 +95,54 @@ public:
         }
 
         return maxDis;
+    }
+
+    int approach2(vector<int>& commands, vector<vector<int>>& obstacles) {
+
+        int x = 0;
+        int y = 0;
+        int dir = 0;  // N = 0, E = 1, S = 2, W = 3.
+        int maxDis = 0;
+        unordered_set<pair<int, int>, pair_hash> st;
+        int dirc[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        
+        for(auto it: obstacles) {
+            int x_i = it[0];
+            int y_i = it[1];
+            st.insert({x_i, y_i});
+        }
+
+        for(int com: commands) {
+            if(com == -1)  // 90 degree Right turn
+                dir = (dir + 1) % 4;
+            else if(com == -2)  // 90 degree Left turn
+                dir = (dir + 3) % 4;
+            else {
+                for(int i = 0; i < com; i++) {
+                    int newX = x + dirc[dir][0];
+                    int newY = y + dirc[dir][1];
+                    
+                    if(st.count({newX, newY}))
+                        break;
+
+                    x = newX;
+                    y = newY;
+                    
+                    maxDis = max(maxDis, ((x * x) + (y * y)));
+                }  
+            }
+        }
+
+        return maxDis;
+    }
+
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        
+        /*
+        return approach1(commands, obstacles);
+        */
+
+        return approach2(commands, obstacles);
     }
 };
