9635af6f67cfdb976451a7c249f9d8b70eb16245
Sandipan pal <palsandipan68@gmail.com>
2026-07-27 09:14:40 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (85.48%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -2,10 +2,33 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         
+        // Process - 1
+        /*
         sort(nums.rbegin(), nums.rend());
 
         int num1 = nums[0] - 1;
         int num2 = nums[1] - 1;
+
+        return (num1 * num2);
+        */
+
+        // Process - 2
+
+        int num1 = 0;
+        int num2 = 0;
+
+        for(int num: nums){
+
+            if(num >= num1){
+                num2 = num1;
+                num1 = num;
+            }
+            else if(num > num2 && num != num1)
+                num2 = num;
+        }
+
+        num1--;
+        num2--;
 
         return (num1 * num2);
     }
