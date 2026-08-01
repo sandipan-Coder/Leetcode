4450baadb5f3550075673655515da17f5647255e
Sandipan pal <palsandipan68@gmail.com>
2026-08-01 10:06:42 +0530

    Time: 83 ms (24.73%) | Memory: 9.5 MB (85.12%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+
+    int solve(int i, int j, vector<int>& nums){
+
+        if(i > j)
+            return 0;
+        
+        if(i == j)
+            return nums[i];
+
+        int take_i = nums[i] + min(solve(i+2, j, nums), solve(i+1, j-1, nums));
+
+        int take_j = nums[j] + min(solve(i, j-2, nums), solve(i+1, j-1, nums));
+
+        return max(take_i, take_j);
+    }
+
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        
+        int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        int player1_score = solve(0, n-1, nums);
+        int player2_score = (sum - player1_score);
+
+        return (player1_score >= player2_score);
+    }
+};
